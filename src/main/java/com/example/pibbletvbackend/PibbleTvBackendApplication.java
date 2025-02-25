b6bd6f459c7391a2d56e86a4d25e610364b1ff7f
HristoGanchev-quad <hristo.ganchev@quadsolutions.nl>
2025-02-25 17:43:49 +0100

    Initialized project

---
+++ src/main/java/com/example/pibbletvbackend/PibbleTvBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.pibbletvbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PibbleTvBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PibbleTvBackendApplication.class, args);
+    }
+
+}
