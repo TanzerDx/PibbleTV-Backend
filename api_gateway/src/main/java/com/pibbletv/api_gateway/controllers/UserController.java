f1c9798e32793ce03bec66b2a7591570a06780f9
TanzerDx <hristoganchev3@gmail.com>
2025-04-04 20:38:11 +0200

    added a getUserById method

--- api_gateway/src/main/java/com/pibbletv/api_gateway/controllers/UserController.java
+++ api_gateway/src/main/java/com/pibbletv/api_gateway/controllers/UserController.java
@@ -1,12 +1,16 @@
 package com.pibbletv.api_gateway.controllers;
 
 import com.pibbletv.api_gateway.dtos.UserDTO;
+import org.springframework.http.HttpStatus;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.security.oauth2.jwt.Jwt;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.reactive.function.client.WebClient;
+import org.springframework.web.server.ResponseStatusException;
 import reactor.core.publisher.Mono;
 
+import java.util.Collections;
+import java.util.List;
 import java.util.Map;
 
 @RestController
@@ -45,6 +49,26 @@ public class UserController {
         String userId = jwt.getSubject();
 
         String userServiceUrl = "/user/getUser?userId=" + userId;
+
+        return this.webClient.get()
+                .uri(userServiceUrl)
+                .retrieve()
+                .bodyToMono(UserDTO.class);
+    }
+
+    @GetMapping("/getUserById")
+    public Mono<UserDTO> getUserById(@RequestParam String id, @AuthenticationPrincipal Jwt jwt) {
+        String requesterId = jwt.getSubject();
+
+        Map<String, Object> realmAccess = jwt.getClaim("realm_access");
+        List<String> roles = realmAccess != null ? (List<String>) realmAccess.get("roles") : Collections.emptyList();
+
+
+        if (!requesterId.equals(id) && !roles.contains("admin")) {
+            throw new ResponseStatusException(HttpStatus.FORBIDDEN, "Access denied");
+        }
+
+        String userServiceUrl = "/user/getUserById?userId=" + id;
 
         return this.webClient.get()
                 .uri(userServiceUrl)
